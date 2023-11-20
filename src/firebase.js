bd1e32e790edea71625d39ea521a0b7f6d2a272a
AdityaGavali <adityagavali18@gmail.com>
2023-11-20 22:20:08 +0530

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
 import {getFirestore}  from 'firebase/firestore'
 const firebaseConfig = {
-  apiKey: "AIzaSyCj1dzpFet7IUhJ0h2UJdXTxDb2aDkuW04",
-  authDomain: "social-media-d1790.firebaseapp.com",
-  projectId: "social-media-d1790",
-  storageBucket: "social-media-d1790.appspot.com",
-  messagingSenderId: "171126594714",
-  appId: "1:171126594714:web:42d67bcc52e5c2ff045323"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket:process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MSID,
+  appId: process.env.APPID
 };
  const app = initializeApp(firebaseConfig);
  const auth = getAuth(app)
